0d487846a69bbee5e734ad675f69d632f2a5c70e
Serj Sintsov <ssivikt@gmail.com>
2013-12-01 19:24:29 +0300

    initial project structure

---
+++ src/main/java/org/jmassivesort/extsort/TwoWayMergeSort.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2013 Serj Sintsov <ssivikt@gmail.com></ssivikt@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jmassivesort.extsort;
+
+import org.jmassivesort.SortingAlgorithm;
+
+/**
+ * @author Serj Sintsov
+ */
+public class TwoWayMergeSort implements SortingAlgorithm {
+
+   private TwoWayMergeSortOptions options;
+
+   public TwoWayMergeSort(TwoWayMergeSortOptions options) {
+      this.options = options;
+   }
+
+   @Override
+   public void apply() {
+
+   }
+
+   public static void main(String[] args) {
+
+   }
+
+}
+
